03b0662df8042c5bb1d604fc2003e253c536ab71
Muneeb <muneeb430430@gmail.com>
2026-07-23 22:41:17 +0500

    Week 1 & Week 2 completed

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("AGNI Industry Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
